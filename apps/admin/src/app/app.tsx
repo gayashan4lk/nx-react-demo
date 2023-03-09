0e56199944fc9504a8abdb6ccf020e62ea07bbc5
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-03-09 13:58:00 +0530

    create basic ui to display games

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,13 +1,49 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
-
 import { Route, Routes, Link } from 'react-router-dom';
+import { getAllGames } from '../fake-api';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@mui/material';
 
 export function App() {
   return (
-    <>
-      <h1>Hi</h1>
-    </>
+    <div className="container">
+      <div className="games-layout">
+        {getAllGames().map((game) => (
+          <Card key={game.id} className="game-card">
+            <CardActionArea>
+              <CardMedia
+                className="game-card-media"
+                image={game.image}
+                title={game.name}
+              />
+              <CardContent>
+                <Typography gutterBottom variant="h5" component="h2">
+                  {game.name}
+                </Typography>
+                <Typography variant="body2" component="p" color="textSecondary">
+                  {game.description}
+                </Typography>
+                <Typography
+                  variant="body2"
+                  component="p"
+                  color="textSecondary"
+                  className="game-rating"
+                >
+                  <strong>Rating:</strong>
+                  {game.rating}
+                </Typography>
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        ))}
+      </div>
+    </div>
   );
 }
 
--- apps/admin/src/fake-api/index.ts
+++ apps/admin/src/fake-api/index.ts
@@ -2,10 +2,19 @@ import type { Game } from '../interfaces/Game';
 
 const games: Game[] = [
   {
+    id: 'settlers-in-the-can',
+    name: 'Settlers In The Can',
+    image: '/assets/can.png',
+    description:
+      'Help your bug family claim the best real estate in a spilled can of beans.',
+    price: 199,
+    rating: Math.random(),
+  },
+  {
     id: 'chess-pie',
     name: 'Chess Pie',
     image: '/assets/chess.png',
-    description: 'A circular game of chess that you can eat as you play',
+    description: 'A circular game of chess that you can eat as you play.',
     price: 15,
     rating: Math.random(),
   },
@@ -13,7 +22,7 @@ const games: Game[] = [
     id: 'purrfection',
     name: 'Purrfection',
     image: '/assets/cat.png',
-    description: 'A cat grooming contest goes horribly wrong',
+    description: 'A cat grooming contest goes horribly wrong.',
     price: 45,
     rating: Math.random(),
   },
