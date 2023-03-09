86a41331a0278fe80e5666aab7afac8ef1be6598
Gayashan Wagachchi <gayashan4lk@gmail.com>
2023-03-09 14:59:31 +0530

    improve component

--- apps/admin/src/app/app.tsx
+++ apps/admin/src/app/app.tsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { Route, Routes, Link } from 'react-router-dom';
 
+import { useState, useEffect } from 'react';
 import { getAllGames } from '../fake-api';
 import {
   Card,
@@ -10,39 +11,64 @@ import {
   Typography,
 } from '@mui/material';
 import styles from './app.module.css';
+import { Game } from '../interfaces/Game';
 
 export function App() {
+  const [games, setGames] = useState<Game[]>();
+  const [loading, setLoading] = useState<boolean>();
+  const [error, setError] = useState<boolean>(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const data = getAllGames();
+    if (data) {
+      setGames(data);
+    } else {
+      setError(true);
+    }
+    setLoading(false);
+  }, []);
+
+  if (loading) return <div>Loading...</div>;
+
+  if (error) return <div>Error occured!</div>;
+
   return (
     <div className="container">
       <div className={styles.gamesLayout}>
-        {getAllGames().map((game) => (
-          <Card key={game.id} className={styles.gameCard}>
-            <CardActionArea>
-              <CardMedia
-                className={styles.gameCardMedia}
-                image={game.image}
-                title={game.name}
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  {game.name}
-                </Typography>
-                <Typography variant="body2" component="p" color="textSecondary">
-                  {game.description}
-                </Typography>
-                <Typography
-                  variant="body2"
-                  component="p"
-                  color="textSecondary"
-                  className={styles.gameRating}
-                >
-                  <strong>Rating:</strong>
-                  {game.rating}
-                </Typography>
-              </CardContent>
-            </CardActionArea>
-          </Card>
-        ))}
+        {games &&
+          games.map((game) => (
+            <Card key={game.id} className={styles.gameCard}>
+              <CardActionArea>
+                <CardMedia
+                  className={styles.gameCardMedia}
+                  image={game.image}
+                  title={game.name}
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    {game.name}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    component="p"
+                    color="textSecondary"
+                  >
+                    {game.description}
+                  </Typography>
+                  <Typography
+                    variant="body2"
+                    component="p"
+                    color="textSecondary"
+                    className={styles.gameRating}
+                  >
+                    <strong>Rating:</strong>
+                    {game.rating}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          ))}
       </div>
     </div>
   );
